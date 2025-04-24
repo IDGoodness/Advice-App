5060f0382207a91c799bb713ea8b84a4a1dcdc3f
Goodness Adewuyi <adewuyigoodness1@gmail.com>
2025-04-24 12:23:50 +0100

    Initial commit, everything ready for building.

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+        <div>
+            <h1 className='text-3xl font-bold underline text-center'>Home</h1>
+            <p className='text-center'>This is the home page</p>
+        </div>
+    
+    </>
+  )
+}
+
+export default Home
