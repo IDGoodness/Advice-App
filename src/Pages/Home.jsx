77c84a3820698ccec67736ee252844932e7c85d0
Goodness Adewuyi <adewuyigoodness1@gmail.com>
2025-04-25 11:11:39 +0100

    Fix.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect} from 'react'
-import Card from '../Components/card'
+import Card from '../Components/Card'
 
 const Home = () => {
     const [advice, setAdvice] = useState(null)
